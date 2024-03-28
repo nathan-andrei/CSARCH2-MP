80a90ecf4896791029a5b4b5ed520e5b83568b78
Mauries Lopez <mauilopez00@gmail.com>
2024-03-28 16:04:31 +0800

    Update commit
    - Binary/Decimal Error Handling
    - Exponent Error Handling
    - Decimal Converter
    - Input Boxes for Binary now only accepts 11 inputs (nag ii-stop kapag greater than 11 ung input)
    - User Interface Design
    - Text Output File

--- back-end.js
+++ back-end.js
@@ -6,6 +6,7 @@ let decimal = doc.querySelector(".Decimal");
 let selector = doc.querySelector("#operation");
 let errText = doc.querySelector("#errText");
 let output = doc.querySelector(".output");
+let exportOutput = doc.querySelector("#export");
 
 //This function will change the operation shown on the site
 selector.addEventListener('change', function() {
@@ -31,18 +32,20 @@ let hexOut = doc.querySelector("#hexOutText");
 
 let convert = doc.querySelector("#convert");
 
+
 //flag variable is for when there's an error in the input
 let out = { binary:"default", hex:"default", flag:false}
 
 //This function will detect if the convert button has been pressed, and will do the correct process
 convert.addEventListener('click', function() {
+
 	if(selector.value == "BI"){
 		[out.binary, out.hex, out.flag] = binaryConvert(bMantissa.value, bExp.value);
 	}
 	else{
 		[out.binary, out.hex, out.flag] = decimalConvert(dMantissa.value, dExp.value);
 	}
-	
+		
 	//binary is 
 	//1 sign
 	//5 exp: +15 bias
@@ -50,7 +53,7 @@ convert.addEventListener('click', function() {
 	//1 00000 1111111111
 	
 	//hex is 4 characters
-	
+		
 	if(out.flag == false){
 		errText.style.display="none"
 		binaryOut.innerHTML=out.binary;
@@ -60,6 +63,51 @@ convert.addEventListener('click', function() {
 	else{
 		output.style.display="none";
 	}
+
+});
+
+//This function will export the output to a text file
+exportOutput.addEventListener('click', function() {
+
+	if ( selector.value == "BI" ){
+
+		let line0 = "========================================\n";
+		let line1 = "[INPUT]" + "\n";
+		let line2 = "Mantissa: " + bMantissa.value + "\n";
+		let line3 = "Exponent: " + bExp.value + "\n";
+		let line4 = "========================================\n";
+		let line5 = "[OUTPUT]" + "\n";
+		let line6 = "Binary: " + out.binary + "\n";
+		let line7 = "Hex: " + out.hex + "\n";
+		let line8 = "========================================\n";
+		let content = line0 + line1 + line2 + line3 + line4 + line5 + line6 + line7 + line8;
+
+		//Credit from https://www.tutorialspoint.com/how-to-create-and-save-text-file-in-javascript
+		var blob = new Blob([content], {
+			type: "text/plain;charset=utf-8",
+		});
+		saveAs(blob, "BinaryConverted.txt");
+
+	} else {
+
+		let line0 = "========================================\n";
+		let line1 = "[INPUT]" + "\n";
+		let line2 = "Mantissa: " + dMantissa.value + "\n";
+		let line3 = "Exponent: " + dExp.value + "\n";
+		let line4 = "========================================\n";
+		let line5 = "[OUTPUT]" + "\n";
+		let line6 = "Binary: " + out.binary + "\n";
+		let line7 = "Hex: " + out.hex + "\n";
+		let line8 = "========================================\n";
+		let content = line0 + line1 + line2 + line3 + line4 + line5 + line6 + line7 + line8;
+
+		var blob = new Blob([content], {
+			type: "text/plain;charset=utf-8",
+		});
+		saveAs(blob, "DecimalConverted.txt");
+
+	}
+
 });
 
 doc.addEventListener('keyup', function(x) { //make the convert button work with enter key
@@ -84,7 +132,6 @@ function binaryConvert(m, e){
 		//we need to check if we're missing digits, if so, figure out if sign or zero extend or trailing zero
 		//We will use trailing zero for now
 		m = extendTrailingZeros(m, 11); //Mantissa should only have 1 - 11 bits including MSb
-		
 		
 		e = parseInt(e, 10) + 15 + expChange;
 		e = decimalToBinary(e, 5);	
@@ -115,11 +162,30 @@ function decimalConvert(m, e){
 	let f = false;
 		
 	if(decimalMantissaErrorCheck(m) && decimalExpErrorCheck(e)){
-		//we need to check if we're missing digits, if so, figure out if sign or zero extend, or trailing zero
-		//Add code here for that
-		if(m < 0){ //negative sign bit
+
+		if(m[0] == '-'){ //negative sign bit;
 			b = "1 ";
 		}
+
+		m = m.replace(/[-+]/g,"");
+		
+		m = decimalToBinaryMantissa(m);
+
+		console.log(m);
+		
+		[ m, expChange ]= normalize(m);
+
+		console.log(m)
+
+		m = extendTrailingZeros(m, 11); //Mantissa should only have 1 - 11 bits including MSb
+		
+		e = parseInt(e, 10) + 15 + expChange;
+		e = decimalToBinary(e, 5);	
+		b = b + e + " ";
+
+		m = m.replace(/1\./g,"");
+		b = b + m; //10 bits
+
 		f = false;
 	}
 	else{
@@ -131,6 +197,17 @@ function decimalConvert(m, e){
 //========================================ERROR CHECKS
 function binaryMantissaErrorCheck(m){ //Binary Mantissa
 	//What are the errors with the mantissa?
+
+	//Check if the mantissa is a binary
+	for ( let i = 0; i < m.length; i++){
+		if ( m[i] > 1 ){
+			//Display error
+			showError("Input is not binary! Please input a binary number.");
+			output.style.display="none";
+			return false;
+		}
+	}
+
 	//Null input
 	if(m == undefined || m == ""){
 		showError("Mantissa is empty!");
@@ -145,13 +222,14 @@ function binaryMantissaErrorCheck(m){ //Binary Mantissa
 		showError("Mantissa is too long!");
 	}
 	//Too many dots or sign symbols
-	else if(m.split(".").length > 2 || m.split("+").length > 2 || m.split("-").length > 2 || m.match(/[+-]|[-+]/g)){
+	else if(m.split(".").length > 2 || m.split("+").length > 2 || m.split("-").length > 2){
 		showError("Invalid format!");
 	}
 	else{
 		return true;
 	}
 	return false;
+
 }
 
 function binaryExpErrorCheck(e){ //Binary Exp
@@ -160,12 +238,17 @@ function binaryExpErrorCheck(e){ //Binary Exp
 	if(e == undefined || e == ""){
 		showError("Exponent is empty!");
 	}
+	else if(e.replace(/\.|[-+]/g, "1").match(/\D/g)){
+		showError("Exponent is not a number!");
+	}
 	//Not a number
 	else if(isNaN(e)){
 		showError("Exponent is not a number!");
 	}
 	//out of range
-	//console.log(error message);
+	else if ( e >= 17 ){
+		showError("Exponent is out of range! Please input an exponent below 17.");
+	}
 	else{
 		return true;
 	}
@@ -177,6 +260,9 @@ function decimalMantissaErrorCheck(m){ //Decimal Mantissa
 	//Null input
 	if(m == undefined || m == ""){
 		showError("Mantissa is empty!");
+	}
+	else if(m.replace(/\.|[-+]/g, "1").match(/\D/g)){
+		showError("Mantissa is not a number!");
 	}
 	//Not a number
 	else if(isNaN(m)){
@@ -196,12 +282,16 @@ function decimalExpErrorCheck(e){ //Decimal Exp
 	if(e == undefined || e == ""){
 		showError("Exponent is empty!");
 	}
+	else if(e.replace(/\.|[-+]/g, "1").match(/\D/g)){
+		showError("Exponent is not a number!");
+	}
 	//Not a number
 	else if(isNaN(e)){
 		showError("Exponent is not a number!");
 	}
-	//out of range
-	//console.log(error message);
+	else if ( e >= 17 ){
+		showError("Exponent is out of range! Please input an exponent below 17.");
+	}
 	else{
 		return true;
 	}
@@ -323,7 +413,82 @@ function decimalToBinary(d, n = 5){
 			binary = "0" + binary;
 		}
 	}
+
+	console.log(binary);
 	return binary;
+}
+
+function decimalToBinaryMantissa(d){
+
+	//Declare variables
+	let m = "";
+	let dotM = "";
+
+	//Get the integer part of the mantissa
+	let tempD = "";
+	for ( let i = 0 ; i < d.length; i++ ){
+
+		if ( d[i] != "."){
+			tempD = tempD.concat(d[i]);
+		}
+		else if ( d[i] == "."){
+			break;
+		}
+
+	}
+
+	//Convert the integer part of the mantissa to binary and store it to m
+	m = ( tempD >>> 0 ).toString(2);
+
+	//Get the fractional part of the mantissa 
+	for ( let i = 0 ; i < d.length; i++ ){
+
+		if ( d[i] == "."){
+			dotM = d.slice(i+1, d.length);
+			break;
+		}
+
+	}
+
+	//Convert the fractional part of the mantissa to binary and store it to dotM
+	dotM = convertDotMantissaToBinary(dotM);
+
+	//Combine the integer and fractional part of the mantissa
+	m = m.concat("." + dotM);
+
+	return m;
+}
+
+function convertDotMantissaToBinary(dotM){
+
+	//Declare variables
+	convertedDotM = "";
+	dotMLength = dotM.length;
+
+	//Make dotM decimal 0.<+dotM>
+	dotM = "0." + dotM;
+
+	let j = 1;
+	for ( let i = 0 ; i < dotMLength; i++ ){
+
+		console.log(convertedDotM)
+
+		if ( (2**-j) > parseFloat(dotM) ){
+			convertedDotM = convertedDotM.concat("0");
+		} else if ( (2**-j) < parseFloat(dotM) ) {
+			convertedDotM = convertedDotM.concat("1");
+			dotM = parseFloat(dotM) - (2**-j);
+		} else if ( (2**-j) == parseFloat(dotM) ) {
+			convertedDotM = convertedDotM.concat("1");
+		} else if ( parseFloat(dotM) <= 0 ){
+			break;
+		} 
+
+		j++;
+	}
+
+	return convertedDotM.replace("0.", "");
+
 }
 
 //This function will extend it with trailing zero up to n number of digits. Will not do anything n is smaller.
@@ -392,3 +557,4 @@ function leadingDigitsNormalize(m, d){
 	}while(m[i] == "0" || m[i] == '.');
 	return [m[i] + '.' + m.substring(i+1, m.length), (i-d)*(-1)]
 }
+
